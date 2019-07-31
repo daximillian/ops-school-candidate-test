871e30c9bacdd8dc0ed0d8ff50bc27b6f36be069
daximillian <nofars@gmail.com>
2019-07-31 16:12:04 +0300

    exercise5 solutions added

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-#add fix to exercise5-server1 here
+# create key for ssh and copy it to server 2
+mkdir -m 700 ~/.ssh/id_rsa
+ssh-keygen -t rsa -P "" -f ~/.ssh/id_dsa
+cat $HOME/.ssh/id_dsa.pub >> $HOME/.ssh/authorized_keys
+ssh-copy-id -i $HOME/.ssh/id_dsa.pub vagrant@server2
+
+# add ssh-agent to deal with the passphrase
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_dsa
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-#add fix to exercise5-server2 here
+# create key for ssh and copy it to server 1
+mkdir -m 700 ~/.ssh/id_rsa
+ssh-keygen -t rsa -P "" -f ~/.ssh/id_dsa
+cat $HOME/.ssh/id_dsa.pub >> $HOME/.ssh/authorized_keys
+ssh-copy-id -i $HOME/.ssh/id_dsa.pub vagrant@server1
+
+# add ssh-agent to deal with the passphrase
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_dsa
